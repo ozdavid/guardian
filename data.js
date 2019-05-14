ccd6a0d53a0f967e9289923e7ec84ee9ad33905f
Yuval Peretz <yuvalphorm@gmail.com>
2019-05-14 17:12:29 +0300

    yuval added json ver1

---
+++ data.js
@@ -0,0 +1,124 @@
+var gdud_js =
+{
+    "id": "battalion9",
+    "destination": "operational",
+    "frequency": "53.00Mhz",
+    "ock": "Agas",
+    "ChildsId": [
+        "company1-9",
+        "company2-9",
+        "magad-9",
+        "samagad-9",
+        "kaman-9"
+    ]
+}
+
+var autonomies = [
+{
+    "id": "company1-9",
+    "ChildsId": [
+        "platoon1-1-9",
+        "platoon2-1-9",
+        "captain-1-9"
+    ],
+    "equipment": [
+        ["122","visible"],
+        ["122","visible"],
+        ["122","visible"],
+        ["710m","visible"]
+    ]
+},
+{
+    "id": "company2-9",
+    "ChildsId": [
+        "platoon1-2-9",
+        "platoon2-2-9",
+        "captain-2-9"
+    ],
+    "equipment": [
+        ["122","visible"],
+        ["122","visible"],
+        ["122","visible"],
+        ["710m","visible"]
+    ]
+},
+{
+    "id": "platoon1-1-9",
+    "ChildsId": [
+    ],
+    "equipment": [
+        ["122","visible"],
+        ["122","visible"],
+        ["72","visible"],
+        ["72","visible"]
+    ]
+}
+,
+{
+    "id": "platoon1-2-9",
+    "ChildsId": [
+    ],
+    "equipment": [
+        ["122","visible"]  ,
+        ["122","visible"],
+        ["72","visible"],
+        ["72","visible"]
+    ]
+},
+{
+    "id": "platoon1-2-9",
+    "ChildsId": [
+    ],
+    "equipment": [
+        ["122","visible"],
+        ["122","visible"],
+        ["72","visible"],
+        ["72","visible"]
+    ]
+}
+,
+{
+    "id": "platoon2-2-9",
+    "ChildsId": [
+    ],
+    "equipment": [
+        ["122","visible"],
+        ["122","visible"],
+        ["72","visible"],
+        ["72","visible"]
+    ]
+},
+{
+    "id": "kaman-9",
+    "ChildsId": [
+    ],
+    "equipment": [
+        ["93","visible"],
+        ["93","visible"],
+        ["122","visible"],
+        ["940","visible"]
+    ]
+}
+{
+    "id": "magad-9",
+    "ChildsId": [
+    ],
+    "equipment": [
+        ["122","visible"],
+        ["93","visible"],
+        ["122","visible"],
+        ["72","visible"]
+    ]
+},
+{
+    "id": "samgad-9",
+    "ChildsId": [
+    ],
+    "equipment": [
+        ["122","visible"],
+        ["93","visible"]
+    ]
+}
+]
+
+
